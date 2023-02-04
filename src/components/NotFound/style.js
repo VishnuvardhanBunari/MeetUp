2b6a947bcc63ac03e63ce60e9ffb0136bb673a59
VishnuvardhanBunari <venkatvishnuvardhanbunari@gmail.com>
2023-02-04 14:33:02 +0000

    first commit

---
+++ src/components/NotFound/style.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export const NotFoundContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+  justify-content: center;
+`
+
+export const NotFoundHeading = styled.h1`
+  color: #64748b;
+  font-size: 32px;
+  font-family: 'Roboto';
+`
+export const NotFoundPara = styled.p`
+  color: #7b8794;
+  font-size: 14px;
+  font-family: 'Roboto';
+`
+
+export const Img = styled.img`
+  height: 250px;
+  width: 250px;
+`
