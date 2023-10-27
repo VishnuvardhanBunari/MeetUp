07fd1d9a8907bde22c9982e91642fa83b3cc1154
VishnuvardhanBunari <89242779+VishnuvardhanBunari@users.noreply.github.com>
2023-10-27 13:47:42 +0530

    Update and rename style.js to styledComponents.js

--- src/components/Home/styledComponents.js
+++ src/components/Home/styledComponents.js
@@ -20,15 +20,15 @@ export const HomePara = styled.p`
 `
 
 export const Button = styled.button`
-  padding-top: 12px;
-  padding-bottom: 12px;
-  padding-left: 20px;
-  padding-right: 20px;
   background-color: #3b82f6;
   color: #ffffff;
   font-family: 'Roboto';
   border: none;
   outline: none;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  padding-left: 20px;
+  padding-right: 20px;
   border-radius: 6px;
 `
 
