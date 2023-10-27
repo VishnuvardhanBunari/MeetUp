b48b4e288be52ce3357d72bd6e704f31b7659e44
VishnuvardhanBunari <89242779+VishnuvardhanBunari@users.noreply.github.com>
2023-10-27 13:53:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,13 +10,36 @@ import RegisterContext from './context/RegisterContext'
 
 import './App.css'
 
+const topicsList = [
+  {
+    id: 'ARTS_AND_CULTURE',
+    displayText: 'Arts and Culture',
+  },
+  {
+    id: 'CAREER_AND_BUSINESS',
+    displayText: 'Career and Business',
+  },
+  {
+    id: 'EDUCATION_AND_LEARNING',
+    displayText: 'Education and Learning',
+  },
+  {
+    id: 'FASHION_AND_BEAUTY',
+    displayText: 'Fashion and Learning',
+  },
+  {
+    id: 'GAMES',
+    displayText: 'Games',
+  },
+]
+
 // These are the lists used in the application. You can move them to any component needed.
 
 // Replace your code here
 class App extends Component {
   state = {
     name: '',
-    topic: 'Arts and Culture',
+    topic: topicsList[0].id,
     isRegistered: false,
     showError: false,
   }
