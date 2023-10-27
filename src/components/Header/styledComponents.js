577e7692e0ea16c48dc49800ed19c51b9cb70559
VishnuvardhanBunari <89242779+VishnuvardhanBunari@users.noreply.github.com>
2023-10-27 13:46:26 +0530

    Update and rename style.js to styledComponents.js

--- src/components/Header/styledComponents.js
+++ src/components/Header/styledComponents.js
@@ -2,10 +2,10 @@ import styled from 'styled-components'
 
 export const Nav = styled.nav`
   display: flex;
+  padding: 20px;
   flex-direction: row;
   align-items: center;
   min-height: 10vh;
-  padding: 20px;
 `
 export const Logo = styled.img`
   height: 50px;
